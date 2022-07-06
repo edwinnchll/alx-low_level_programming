bb7630edc351c23362f48a68d9943b8149d753d8
edwinnchll <edwinnchll@gmail.com>
2022-07-06 01:42:31 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<main.h>
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+ **/
+int main(void)
+{
+   char edwin[] = "_putchar";
+   int i=0;
+   for(i;i<8;i++){
+   
+   _putchar(edwin[i]);
+   }
+   _putchar("/n");
+   return (0);
+
+}
