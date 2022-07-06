8247c0bfb73d741a58fda09eca4626bf3373a09e
edwinnchll <edwinnchll@gmail.com>
2022-07-06 02:53:20 +0000

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-        _putchar('_');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -17,7 +17,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
-   return (0);
+	return (0);
 
 }
