dfd0e46c1af1433680372211729abc860a023414
edwinnchll <edwinnchll@gmail.com>
2022-09-02 00:34:16 +0000

    Editted

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,21 +13,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int err, len, fileDescriptor;
+	int o, w, len = 0;
 
-	err = len = fileDescriptor = 0;
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fileDescriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-		if (fileDescriptor < 0)
-			return (-1);
-	while (text_content && text_content[len])
-		len++;
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-	err = write(fileDescriptor, text_content, len);
-	if (err < 0)
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
-	close(fileDescriptor);
+
+	close(o);
+	
 	return (1);
 }
